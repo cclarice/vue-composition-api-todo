4ff6c1a96aa461fec185e8447b9aa9cf37935c1a
cclarice <179danil@gmail.com>
2022-03-16 12:25:15 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <main>
+    <task-input @onAddTask="addTask"/>
+    <ul class="my-list task-list">
+      <li v-for="item in taskList" :key="item.id">
+        <task-card :model="item"
+                   @onRemove="removeTask(item.id)"
+                   @onDone="setDoneTask(item.id)"/>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+import TaskInput from './components/TaskInput.vue'
+import TaskCard from './components/TaskCard.vue'
+import { ref } from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    TaskCard,
+    TaskInput
+  },
+  setup () {
+    const taskList = ref([{
+        id: 0,
+        title: 'Create video',
+        description: 'And upload on YouTube',
+        status: false
+      }])
+
+    const addTask = ({ title, description }) => {
+      taskList.value = [
+        ...taskList.value,
+        {
+          id: taskList.value[taskList.value.length - 1].id + 1,
+          title, description,
+          status: false
+        }
+      ]
+    }
+
+    const setDoneTask = (id) => {
+      taskList.value = taskList.value.map(x => {
+        if (x.id === id) {
+          x.status = true
+        }
+        return x
+      })
+    }
+
+    const removeTask = (id) => {
+      taskList.value = taskList.value.filter(x => x.id !== id)
+    }
+
+    return {
+      taskList,
+      addTask,
+      setDoneTask,
+      removeTask
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .task-list {
+    list-style: none;
+  }
+</style>
---
+++ src/components/TaskInput.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="task-input my-list">
+    <input v-model="title" placeholder="Title" type="text">
+    <input v-model="description" placeholder="Description" type="text">
+    <button @click="onAddTask" >➕ 📃</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  // *
+  // data () {
+  //   return {
+  //     title: '',
+  //     description: ''
+  //   }
+  // },
+  emits: {
+    onAddTask ({ title, description }) {
+      if (title === '' || description === '') {
+        alert('Fill some info please!')
+        return false
+      }
+      return true
+    }
+  },
+  setup (props, { emit }) {
+    // *
+    const title = ref('')
+    const description = ref('')
+
+    const onAddTask = () => {
+      emit('onAddTask', { title: title.value, description: description.value })
+    }
+
+    return {
+      title,
+      description,
+      onAddTask
+    }
+  }
+}
+</script>
+
+<style scoped>
+.task-input {
+  margin-bottom: 10px;
+}
+</style>
